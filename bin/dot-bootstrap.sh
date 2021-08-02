2d2691ea8cd362d4c54a338c0c65bdf595e6eab5
Philipp Bartsch <tastybug@tastybug.com>
2021-08-02 17:35:27 +0200

    Auto-install ansible on debian if missing

--- bin/dot-bootstrap.sh
+++ bin/dot-bootstrap.sh
@@ -12,7 +12,14 @@ if ! [ -x "$(command -v ansible)" ]; then
 fi
 
 # no darwin task needs elevated privileges
-if [[ "$OSTYPE" != "darwin"* ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if ! [ -x "$(command -v ansible)" ]; then
+    brew install ansible
+  fi
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  if ! [ -x "$(command -v ansible)" ]; then
+    sudo apt-get update && sudo apt-get install ansible
+  fi 
   ASK_PASS='--ask-become-pass'
 fi
 
