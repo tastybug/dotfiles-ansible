5a4cbaf3d15af9225aa9289f09ddbf70930be1b6
Philipp Bartsch <tastybug@tastybug.com>
2021-08-02 17:21:21 +0200

    Do not ask for sudo pw on Macos

--- bin/dot-bootstrap.sh
+++ bin/dot-bootstrap.sh
@@ -11,6 +11,11 @@ if ! [ -x "$(command -v ansible)" ]; then
   brew install ansible
 fi
 
-ansible-playbook --ask-become-pass -i ~/.dotfiles/hosts ~/.dotfiles/dotfiles.yml --tags $tags
+# no darwin task needs elevated privileges
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  ASK_PASS='--ask-become-pass'
+fi
+
+ansible-playbook $ASK_PASS -i ~/.dotfiles/hosts ~/.dotfiles/dotfiles.yml --tags $tags
 
 echo "Done"
