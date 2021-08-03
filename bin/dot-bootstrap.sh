6eda900320414ee3a655bbcb4f4d11e969f613e4
Philipp Bartsch <tastybug@tastybug.com>
2021-08-03 09:41:08 +0200

    Silent installation of ansible

--- bin/dot-bootstrap.sh
+++ bin/dot-bootstrap.sh
@@ -14,7 +14,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   fi
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   if ! [ -x "$(command -v ansible)" ]; then
-    sudo apt-get update > /dev/null && sudo apt-get -y install ansible
+    sudo apt-get --quiet update > /dev/null && sudo apt-get --yes --quiet install ansible
   fi 
   ASK_PASS='--ask-become-pass'
 fi
