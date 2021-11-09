c226d2e13a998f60ecbb8e359e807072f3a5b927
Philipp Bartsch <tastybug@tastybug.com>
2021-11-09 16:35:37 +0100

    Reenable kubectl completion

--- roles/zsh/completion.zsh
+++ roles/zsh/completion.zsh
@@ -1,1 +1,3 @@
-# source <(kubectl completion zsh)
+# https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-zsh/
+# for aliases, there needs to be an extra step
+source <(kubectl completion zsh)
