51a7fe6fd620f5a7b7bc10f10aefb0b58c17e39d
Philipp Bartsch <tastybug@tastybug.com>
2023-04-14 00:55:00 +0200

    .

--- roles/zsh/completion.zsh
+++ roles/zsh/completion.zsh
@@ -1,3 +1,4 @@
 # https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-zsh/
 # for aliases, there needs to be an extra step
 source <(kubectl completion zsh)
+source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
--- roles/zsh/path.zsh
+++ roles/zsh/path.zsh
@@ -3,3 +3,4 @@ export PATH=$PATH:/$HOME/.dotfiles/bin
 export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$HOME/.dotnet/tools
+source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
