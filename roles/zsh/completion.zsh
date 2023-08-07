e0cbdf4bb0d82dd7a15b7685238b1f5fd95bf656
Philipp Bartsch <pbartsch@wayfair.com>
2023-08-07 10:30:55 +0200

    fix order of imports in zsh and move shell completion out of the project

--- roles/zsh/completion.zsh
+++ roles/zsh/completion.zsh
@@ -1,4 +1,7 @@
 # https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-zsh/
 # for aliases, there needs to be an extra step
-source <(kubectl completion zsh)
-source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+#
+# put the following 2 into completion_external
+# source <(kubectl completion zsh)
+# source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+~
